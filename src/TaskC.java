47e67e41629ac3abe4b8f5cddbf67af58e5f71eb
sanndmit <100490786+sanndmit@users.noreply.github.com>
2023-03-21 23:50:20 +0300

    final

--- src/TaskC.java
+++ src/TaskC.java
@@ -2,7 +2,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.Map;
 import java.util.Set;
 
 public class TaskC {
@@ -14,7 +19,6 @@ public class TaskC {
 
         int[][] orders = new int[n + 1][2];
         Set<int[]> variants = new HashSet<>();
-        Set<int[]> previous = null;
 
         String[] fs = reader.readLine().split(" ");
         int f = Integer.parseInt(fs[0]);
@@ -22,23 +26,18 @@ public class TaskC {
         orders[1][0] = f;
         orders[1][1] = s;
 
-        int[] vf = new int[2];
-        vf[0] = f;
-        vf[1] = 0;
-        variants.add(vf);
+        variants.add(variant(f, 0));
+        variants.add(variant(0, s));
 
-        int[] vs = new int[2];
-        vs[0] = 0;
-        vs[1] = s;
-        variants.add(vs);
-
-        int[][] allocation = new int[n * 1000 + 1][n * 1000 + 1];
-        allocation[f][0] = 1;
-        allocation[0][s] = 1;
+        List<Map<Integer, Integer>> layers = new ArrayList<>(n + 1);
+        layers.add(null);
+        layers.add(new HashMap<>());
+        layers.get(1).put(f, 0);
+        layers.get(1).put(0, s);
 
         for (int i = 2; i <= n; i++) {
 
-            previous = variants;
+            Set<int[]> previous = variants;
             variants = new HashSet<>();
 
             fs = reader.readLine().split(" ");
@@ -47,55 +46,69 @@ public class TaskC {
             orders[i][0] = f;
             orders[i][1] = s;
 
+            Map<Integer, Integer> data = new TreeMap<>();
             for (int[] v : previous) {
 
-                vf = new int[2];
-                vf[0] = v[0] + f;
-                vf[1] = v[1];
-                if (allocation[v[0] + f][v[1]] < i) {
-                    allocation[v[0] + f][v[1]] = i;
-                    variants.add(vf);
-                }
+                int[] vf = variant(v[0] + f, v[1]);
+                if (!data.containsKey(vf[0]) || data.get(vf[0]) > vf[1])
+                    data.put(vf[0], vf[1]);
 
-                vs = new int[2];
-                vs[0] = v[0];
-                vs[1] = v[1] + s;
-                if (allocation[v[0]][v[1] + s] < i) {
-                    allocation[v[0]][v[1] + s] = i;
-                    variants.add(vs);
-                }
+                int[] vs = variant(v[0], v[1] + s);
+                if (!data.containsKey(vs[0]) || data.get(vs[0]) > vs[1])
+                    data.put(vs[0], vs[1]);
 
             }
+
+            Map<Integer, Integer> layer = new HashMap<>();
+            int ceiling = Integer.MAX_VALUE;
+            for (Map.Entry<Integer, Integer> e : data.entrySet())
+                if (e.getValue() < ceiling) {
+                    ceiling = e.getValue();
+                    int[] v = variant(e.getKey(), e.getValue());
+                    variants.add(v);
+                    layer.put(v[0], v[1]);
+                }
+            layers.add(layer);
+
+            System.out.println(layer.size());
 
         }
         reader.close();
 
-        int fr = n * 1000;
-        int sr = n * 1000;
-        int maxr = n * 1000;
-        for (int i = 0; i <= maxr; i++)
-            for (int j = 0; j <= maxr; j++)
-                if (allocation[i][j] == n && Math.max(i, j) < maxr) {
-                    fr = i;
-                    sr = j;
-                    maxr = Math.max(i, j);
-                }
+        int[] result = variant(Integer.MAX_VALUE, Integer.MAX_VALUE);
+        for (int[] v : variants)
+            if (Math.max(v[0], v[1]) < Math.max(result[0], result[1]))
+                result = v;
 
-        String result = "";
-        for (int i = n; i > 1; i--)
-            if (fr - orders[i][0] >= 0 && allocation[fr - orders[i][0]][sr] == i - 1) {
-                fr -= orders[i][0];
-                result = "1 " + result;
+        System.out.println(result[0]);
+        System.out.println(result[1]);
+
+        String output = "";
+        for (int i = n; i > 1; i--) {
+            Map<Integer, Integer> previous = layers.get(i - 1);
+            if (result[1] - previous.getOrDefault(result[0], 0) == orders[i][1]) {
+                result[1] -= orders[i][1];
+                output = "2 " + output;
             } else {
-                sr -= orders[i][1];
-                result = "2 " + result;
+                result[0] -= orders[i][0];
+                output = "1 " + output;
             }
+        }
 
-        result = ((fr > 0) ? "1" : "2") + " " + result;
+        output = ((result[0] > 0) ? "1" : "2") + " " + output;
 
         FileWriter writer = new FileWriter("output.txt");
-        writer.write(result);
+        writer.write(output);
         writer.close();
+
+    }
+
+    static int[] variant(int first, int second) {
+
+        int[] result = new int[2];
+        result[0] = first;
+        result[1] = second;
+        return result;
 
     }
 
